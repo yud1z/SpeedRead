6ee5b444b8cd3ab29c59cf0e70166eab1429aab8
yudis <yudis.net42@gmail.com>
2015-12-18 16:30:43 +0700

    adding new label info file name

--- asset/js/main.js
+++ asset/js/main.js
@@ -39,4 +39,30 @@ $(function(){
       $('#button_input_file').click(); 
     });
 
+  function handleFileSelect(evt) {
+    var files = evt.target.files; // FileList object
+
+    // files is a FileList of File objects. List some properties.
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                  f.size, ' bytes, last modified: ',
+                  f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
+                  '</li>');
+    }
+    document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+  }
+
+  $('#button_input_file').change(function(evt){
+    var files = evt.target.files; // FileList object
+
+    var output = [];
+    for (var i = 0, f; f = files[i]; i++) {
+      $('#info_file_name').html(f.name);
+    }
+
+
+  });
+
+
 });
