0b19f8c510000cf705b68beb4f93fd91512db787
yudis <yudis.net42@gmail.com>
2015-12-18 15:52:19 +0700

    toggle button

--- asset/js/main.js
+++ asset/js/main.js
@@ -5,10 +5,21 @@ $(function(){
       $('#button_pause').show(); 
     });
 
-
     $('#button_pause').click(function(){
       $('#button_pause').hide(); 
       $('#button_play').show(); 
+    });
+
+
+    $('#button_expand').click(function(){
+      $('#button_expand').hide(); 
+      $('#button_compress').show(); 
+    });
+
+
+    $('#button_compress').click(function(){
+      $('#button_compress').hide(); 
+      $('#button_expand').show(); 
     });
 
 });
